3c59e46de5e54ea84d6b55f2336f23e28ef04d73
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-09-11 22:29:52 -0300

    feat: global styles

---
+++ src/Styles/global.jsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+        font-family:monospace;
+    }
+
+    a{
+        text-decoration:none;
+    }
+
+    button{
+        border:none;
+        outline:none;
+        cursor:pointer;
+    }
+`;
